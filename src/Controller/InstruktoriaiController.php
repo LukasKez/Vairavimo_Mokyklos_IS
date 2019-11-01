1af60964055179ef0a729924e71c3c68a6cc5246
LukasRimk <lukas.rmk@gmail.com>
2019-11-01 14:33:14 +0200

    Added Instruktoriai GUI

---
+++ src/Controller/InstruktoriaiController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class InstruktoriaiController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('instruktoriai/instruktoriai.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/instruktoriai/prideti", name="app_instruktoriaiPrideti")
+     */
+    public function add()
+    {
+        return $this->render('instruktoriai/prideti.html.twig', [
+        'purpose' => 'Prideti'
+        ]);
+    }
+
+    /**
+     * @Route("/instruktoriai/redaguoti", name="app_instruktoriaiRedaguoti")
+     */
+    public function edit()
+    {
+        return $this->render('instruktoriai/prideti.html.twig', [
+        'purpose' => 'Redaguoti',
+        ]);
+    }
+
+    /**
+     * @Route("/instruktoriai/istrinti", name="app_instruktoriaiIstrinti")
+     */
+    public function delete()
+    {
+
+        return $this->render('instruktoriai/instruktoriai.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/instruktoriai/profilis", name="app_instruktoriaiProfilis")
+     */
+    public function profile()
+    {
+
+
+        return $this->render('instruktoriai/profilis.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/instruktoriai/instr", name="app_instruktoriaiInstruktoriai")
+     */
+    public function salary()
+    {
+
+
+        return $this->render('instruktoriai/instruktoriai.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/instruktoriai/filialoID", name="app_instruktoriaiTvarkarastis")
+     */
+    public function tvarkarastis()
+    {
+
+
+        return $this->render('instruktoriai/tvarkarastis.html.twig', [
+
+        ]);
+    }
+}
